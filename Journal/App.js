a9305e22fc32857ebbf0f5b3db245fb8a5942f8e
s-vali <valiante.sofia@gmail.com>
2024-04-12 19:18:45 -0400

    Guided Journal Page
    1. Static code for Guided Journal page
    2. Arrow svg added to main app page
    3. 'guided-journal' folder added for Guided Journal Page
    4. Images added to assets/images and assets/svg

--- Journal/App.js
+++ Journal/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Svg, { Path } from 'react-native-svg';
+
+import GuidedJournalPage from "./guided-journal/GuidedJournalPage";
+
+const PlaceholderImage = require('./assets/images/file.png');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.main}>
+      <Svg 
+        style={styles.svg}
+        width={40}
+        height={40}
+        viewBox="0 0 20 20"
+        stroke="#744C4C"
+        strokeLinecap="round" 
+        strokeLinejoin="round"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        strokeWidth={2}
+      >
+        <Path d="M6 12H18M6 12L11 7M6 12L11 17"/>
+      </Svg>
+      <View style={styles.container}>
+        <GuidedJournalPage />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  main: {
+    backgroundColor: '#FFF6EF',
   },
+  svg: {
+    marginTop: 50,
+    marginLeft: 10,
+  }
 });
