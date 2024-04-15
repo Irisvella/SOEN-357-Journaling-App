ee2c2a6fd32bd2aff932305ba8cec07fef4bb448
Fatoumatabintabarry <97992954+Fatoumatabintabarry@users.noreply.github.com>
2024-04-15 15:09:47 -0400

    Update welcomepage

--- Journal/src/Screens/WelcomePage.js
+++ Journal/src/Screens/WelcomePage.js
@@ -10,142 +10,154 @@ import {
 
 function WelcomePage({ navigation }) {
   return (
-    <ScrollView style={styles.background}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.header}>Welcome back SARAH!</Text>
-        
+    <ScrollView style={styles.scrollViewContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome back SARAH !</Text>
+
         <Image
-        source={require("../../assets/images/welcome.png")}
-        style={styles.headerbox}
-      >
-        
-      </Image>
-      </View>
-     
+          source={require("../../assets/images/welcome.png")}
+          style={styles.headerbox}
+        />
 
-      <View style={styles.guidedPromptsContainer}>
-        {/* Here you can map through your prompts or manually place them as needed */}
-        
-        <Text style={styles.header}>Guided Prompts</Text>
-       
-        <View style={styles.prompt}>
-          <Text style={styles.promptText}>I write this for...</Text>
+        <Text style={styles.sectionTitle}>Guided Prompts</Text>
+        <View style={styles.entryContainer} >
+
+        {/* This is where you would map through your entries */}
+        <TouchableOpacity style={styles.entry} onPress={() => navigation.navigate("JournalEntryPageScreen")}>
+          <Text style={styles.entryTitle}>Today's journal entry</Text>
+          <Text>April 5th, 2024</Text>
+          <Text style={styles.hashtags}>vacation #bless #camping</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.entry} onPress={() => navigation.navigate("JournalEntryPageScreen")}>
+          <Text style={styles.entryTitle}>Today's journal entry</Text>
+          <Text>April 5th, 2024</Text>
+          <Text style={styles.hashtags}>vacation #bless #camping</Text>
+        </TouchableOpacity>
         </View>
-        <View style={styles.prompt1}>
-          <Text style={styles.promptText}>This is the journal...</Text>
+    
+        
+        
+        <Text style={styles.sectionTitle}>Start a new one</Text>
+          <TouchableOpacity
+            style={styles.fab}
+            onPress={() => navigation.navigate("JournalEntryPageScreen")}
+          >
+            <Text style={styles.fabIcon}>+</Text>
+          </TouchableOpacity>
+        
+
+        {/* Navigation Links */}
+        <View style={styles.navigationLinks}>
+          <TouchableOpacity onPress={() => navigation.navigate("Goals")}>
+            <Text>Goals</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("JournalEntriesScreen")}>
+            <Text>Journal Entries</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("MoodTracker")}>
+            <Text>Mood Tracker</Text>
+          </TouchableOpacity>
         </View>
-        {/* More prompts as needed */}
-      </View>
-
-      <TouchableOpacity
-        style={styles.newEntryButton}
-        onPress={() => navigation.navigate("NewEntry")}
-      >
-        <Text style={styles.newEntryButtonText}>Start a new one</Text>
-      </TouchableOpacity>
-
-      <View style={styles.navBar}>
-        <TouchableOpacity onPress={() => navigation.navigate("Goals")}>
-          <Text>Goals</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('JournalEntriesScreen')}>
-          <Text>Journal Entries</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("MoodTracker")}>
-          <Text>Mood Tracker</Text>
-        </TouchableOpacity>
+        
+        
       </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    
+  scrollViewContainer: {
     backgroundColor: "#FFF6EF",
   },
-  headerContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 50,
-    paddingBottom: 20,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 80, // Adjust the padding as needed to ensure FAB doesn't overlap content
   },
-
-  headerbox: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 50,
-    paddingBottom: 20,
-    width:600,
-    height:300,
-    borderRadius:15,
-
-  },
-  header: {
-    fontSize: 24,
+  title: {
+    fontSize: 26,
+    color: "#563d2d",
     fontWeight: "bold",
-    color: "#744C4C",
+    marginTop: 40,
+    alignSelf: "center",
+  },
+  sectionTitle: {
+    fontSize: 20,
+    color: "#563d2d",
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    marginLeft: 20,
     marginTop: 20,
-    marginTop: 50,
-    marginBottom: 20,
   },
-  subtitle: {
-    fontSize: 16,
-    color: "#744C4C",
-    textAlign: "center",
+  hashtags: {
+    color: "#DE3163",
+    fontWeight: "bold",
+  },
+  entryTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  headerbox: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'contain',
+    borderRadius: 15,
+    marginTop: 20,
+  },
+  entryContainer: {
+    width: '100%',
+    flexDirection: "column",
+   
+  },
+  entry: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    padding: 20,
     marginHorizontal: 20,
-    marginBottom: 20,
-  },
-  guidedPromptsContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    margin: 20,
-  },
-  prompt: {
-    flexDirection: "column",
-    backgroundColor: '#FAD0C5',
-    borderRadius: 16,
-    padding: 15,
-    margin: 8,
-    shadowColor: '#744C4C',
+    marginTop: 10,
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
+    shadowRadius: 6,
+    shadowOpacity: 0.1,
+    elevation: 3,
   },
-  prompt1: {
-    flexDirection: "column",
-    backgroundColor: '#FAD0C5',
-    borderRadius: 16,
-    padding: 15,
-    margin: 8,
-    shadowColor: '#744C4C',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-  },
-
-
-  promptText: {
-    fontSize: 16,
-    color: "#744C4C",
-  },
-  newEntryButton: {
-    backgroundColor: "#DAA520", // Replace with your desired color
-    borderRadius: 25,
-    padding: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    marginVertical: 20,
-  },
-  
-  navBar: {
+  navigationLinks: {
+    
+    marginTop: 20,
     backgroundColor: "#DAA520", 
     flexDirection: "row",
+
     justifyContent: "space-around",
-    paddingVertical: 10,
-    borderTopWidth: 1,
+    gap:20,
+    padding: 15,
     borderTopColor: "#EEE",
-    
+    borderRadius: 20,
+
+  },
+  fabContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  fab: {
+    backgroundColor: "#563d2d",
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "brown",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.1,
+    elevation: 3,
+  },
+  fabIcon: {
+    fontSize: 30,
+    color: "white",
   },
 });
 
